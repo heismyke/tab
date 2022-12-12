deed323292b231a07fd7934cde588ef98b78a1d3
myke <81974207+heismyke@users.noreply.github.com>
2022-12-12 14:20:48 +0100

    all

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}'],
+  
+  content: [
+    './public/index.html',
+    './*.{html,js}'
+],
   theme: {
     extend: {},
   },
